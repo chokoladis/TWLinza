04e5667e15ac23304eb6c52cbe3c6af90da80a64
chokoladis <igor2011__igor@mail.ru>
2023-02-08 13:33:45 +0400

    v0.31

--- third/modules/currencies_module/lang/ru/install/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$MESS['MYMODULE_MODULE_NAME'] = 'currencies';
-$MESS['MYMODULE_MODULE_DESCRIPTION'] = 'Модуль сделан в рамках тестового задания в Linza ITeam';
-$MESS['MYMODULE_MODULE_PARTNER_NAME'] = 'название партнера';
